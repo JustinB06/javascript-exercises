cc6daf3f89e50b113a2f0537c1c8f398e5c7799c
JustinB06 <practice@test.com>
2025-09-24 19:09:03 -0400

    Complete exercise 07_reverseString
    Completed the function reverseString() and passed
    all tests.
    
    Apparently, in JavaScript, strings cannot be
    directly reversed. I assume this refers to there
    being no implemented method to do so. Thus,
    to reverse a string, we must:
    1. Convert the string into an array.
    2. Reverse this new array.
    3. Join the array into a string, which we then
    return.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function (stringToReverse) {
+  const stringCharArray = stringToReverse.split("");
+  //stringCharArray.reverse();
+  return stringCharArray.reverse().join("");
 };
 
 // Do not edit below this line
