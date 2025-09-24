0e655b499eb9d28ba63dfeea43f45227185e66ba
JustinB06 <practice@test.com>
2025-09-24 18:42:42 -0400

    Complete exercise 06_repeatString
    The repeatString() function was completed and all
    tests have passed.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function (stringToRepeat, timesToRepeat) {
+  let builtString = "";
 
+  if (timesToRepeat < 0) return "ERROR";
+
+  for (let i = 0; i < timesToRepeat; i++) {
+    builtString += stringToRepeat;
+  }
+  return builtString;
 };
 
 // Do not edit below this line
