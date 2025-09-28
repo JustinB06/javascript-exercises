227c2627c785df086ed7d419efdfcf35e0544130
JustinB06 <practice@test.com>
2025-09-28 15:38:27 -0400

    Complete exercise 08_removeFromArray.js
    Completed the exercise through learning to use
    "rest parameters" for functions. They allow a
    function to accept an indefinite number of
    arguments, which are then stored in an array.
    
    Similar looking, but different, is spread
    syntax (...). It allows you to expand an iterable
    data type. This expanded form might become a
    comma-seperated list, or an indexed list, or maybe
    something else too?

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (inputArray, ...toRemove) {
+  // Loop through elements that need to be removed
+  for (elementToRemove of toRemove) {
+    while (inputArray.includes(elementToRemove)) {
+      let indexToRemove = inputArray.indexOf(elementToRemove);
+      inputArray.splice(indexToRemove, 1);
+    }
+  }
+  return inputArray;
 };
 
 // Do not edit below this line
