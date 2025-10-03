33d7072abf7063a58ad72591e8265161a008475c
JustinB06 <practice@test.com>
2025-10-02 20:28:00 -0400

    Complete exercise 11_tempConversion
    Completed the exercise utilizing Math.round() to
    round decimal numbers.
    
    Note that Math.round() always rounds a number to
    the nearest integer. Thus, to preserve decimal
    digits, we must multiply the number before
    conversion and divide it after conversion
    accordingly.

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheitTemp) {
+  let celsiusTemp = (fahrenheitTemp - 32) * (5 / 9);
+
+  /* Math.round() always rounds a number to the 
+  nearest integer, so we multiply and divide by
+  10 to preserve 1 decimal place.*/
+  celsiusTemp = Math.round(celsiusTemp * 10) / 10;
+  return celsiusTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsiusTemp) {
+  let fahrenheitTemp = celsiusTemp * (9 / 5) + 32;
+  fahrenheitTemp = Math.round(fahrenheitTemp * 10) / 10;
+  return fahrenheitTemp;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
