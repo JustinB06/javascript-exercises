e2c837b634931a104466c9857d4e61fb0f88e052
JustinB06 <practice@test.com>
2025-11-07 19:55:05 -0500

    Complete exercise 13_palindromes
    Used a regex approach, but the solution didn't.
    The solution used a combination of string methods
    and array methods. The array methods allowed the
    splitting and joining of the string
    character-by-character. Additionally, they
    allowed the use of ".filter()", to remove
    punctuational characters from the string.
    
    The ".filter()" method used a callback function
    involving the "str.includes()" method. A string
    named "alpahanumerical" stored characters [a-z]
    and [0-9]. When running
    "alphanumerical.includes(element)", it acts
    similarly to a regex matching method.
    
    Instead of using pointers to check if the
    string is a palindrome, they simply reversed
    the "cleaned" string and checked if it was
    equal to the non-reversed string.
    
    Also changed the local email used for git.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,30 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  /* Regex: 
+    - The characters we match are
+    indicated by the inner "/ /"
+    
+    - The "g" is a global flag, meaning that
+    regex will attempt to continue to search
+    the string after the first valid match.
+    */
+  const regex = /\w/g;
 
+  /* Using the regex pattern, str.match()
+    will return an array of individual 
+    string characters that matched the pattern. */
+  let filteredArrayString = str.match(regex);
+  let filteredString = filteredArrayString.join("");
+  filteredString = filteredString.toLowerCase();
+
+  let left = 0;
+  let right = filteredString.length - 1;
+  while (left < right) {
+    if (filteredString[left] !== filteredString[right]) return false;
+    left++;
+    right--;
+  }
+
+  return true;
 };
 
 // Do not edit below this line
